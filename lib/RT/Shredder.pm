8702baaa6772950166b3d6e366c8f6e5c36afe9d
Alex Vandiver <alexmv@bestpractical.com>
2011-08-16 14:59:26 -0400

    Merge RT::Shredder::Record into RT::Record

--- lib/RT/Shredder.pm
+++ lib/RT/Shredder.pm
@@ -224,8 +224,6 @@ BEGIN {
 
     require RT;
 
-    require RT::Shredder::Record;
-
     require RT::Shredder::ACE;
     require RT::Shredder::Attachment;
     require RT::Shredder::CachedGroupMember;
