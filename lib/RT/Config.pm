2499d7e5d6ca62b4697d20469d61e2df24b7d620
Alex Vandiver <alexmv@bestpractical.com>
2014-01-02 21:37:16 -0500

    Use HTML::FormatExternal to allow external HTML → text formatters
    The HTML::FormatText::WithLinks::AndTables module is unmaintained, and
    contains multiple bugs which trigger runtime errors.  Add
    HTML::FormatExternal as an optional dependency for slower but more
    reliable HTML to text conversion using external text-based web browsers
    and text conversion tools.

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -635,6 +635,10 @@ our %META;
             $self->Set( MailCommand => 'sendmailpipe' );
         },
     },
+    HTMLFormatter => {
+        Type => 'SCALAR',
+        PostLoadCheck => sub { RT::Interface::Email->_HTMLFormatter },
+    },
     MailPlugins  => {
         Type => 'ARRAY',
         PostLoadCheck => sub {
